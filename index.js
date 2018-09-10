aefd075b78e4071da09ab859fe9f8be2d68c34b6
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-10 18:16:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  const nest = document
+    .getElementById('nested')
+    .quarySelector('div div div div.target')
+  return nest;
+}
+
+
+function increaseRankBy(n) {
+  const list = document
+    .getElementById('app')
+    .quarySelector('ul.ranked-list')
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(i + n);
+  }
+}
+
+function deepestChild() {
+  
+}
